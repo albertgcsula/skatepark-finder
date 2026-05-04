707551f8f9f7d26cd48e708fd3780a2dd3806e03
Albert Gutierrez <albertgcsula@gmail.com>
2026-05-03 21:23:50 -0700

    Fix: move providers inside router context

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider } from '@tanstack/react-router'
 import { getRouter } from './router'
-import { ThemeProvider, CssBaseline, Container } from '@mui/material'
-import { CacheProvider } from '@emotion/react'
-import theme from './theme'
-import createEmotionCache from './createEmotionCache'
-import { SkateparkProvider } from './context/SkateparkContext'
 
 import '@fontsource/roboto/300.css'
 import '@fontsource/roboto/400.css'
@@ -15,7 +10,6 @@ import '@fontsource/roboto/700.css'
 import './styles.css'
 
 const router = getRouter()
-const emotionCache = createEmotionCache()
 
 const rootElement = document.getElementById('app')!
 
@@ -23,16 +17,7 @@ if (!rootElement.innerHTML) {
   const root = ReactDOM.createRoot(rootElement)
   root.render(
     <React.StrictMode>
-      <CacheProvider value={emotionCache}>
-        <ThemeProvider theme={theme}>
-          <CssBaseline />
-          <SkateparkProvider>
-            <Container maxWidth="lg" sx={{ py: 4 }}>
-              <RouterProvider router={router} />
-            </Container>
-          </SkateparkProvider>
-        </ThemeProvider>
-      </CacheProvider>
+      <RouterProvider router={router} />
     </React.StrictMode>,
   )
 }
--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,5 +1,12 @@
 import { Outlet, createRootRoute } from '@tanstack/react-router'
 import { TanStackRouterDevtools } from '@tanstack/react-router-devtools'
+import { ThemeProvider, CssBaseline, Container } from '@mui/material'
+import { CacheProvider } from '@emotion/react'
+import theme from '../theme'
+import createEmotionCache from '../createEmotionCache'
+import { SkateparkProvider } from '../context/SkateparkContext'
+
+const clientSideEmotionCache = createEmotionCache()
 
 export const Route = createRootRoute({
   component: RootComponent,
@@ -7,9 +14,16 @@ export const Route = createRootRoute({
 
 function RootComponent() {
   return (
-    <>
-      <Outlet />
+    <CacheProvider value={clientSideEmotionCache}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <SkateparkProvider>
+          <Container maxWidth="lg" sx={{ py: 4 }}>
+            <Outlet />
+          </Container>
+        </SkateparkProvider>
+      </ThemeProvider>
       <TanStackRouterDevtools position="bottom-right" />
-    </>
+    </CacheProvider>
   )
 }
